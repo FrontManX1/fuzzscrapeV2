2f4856ade2b9e4bea233b77e024c7daa61b26e55
FrontManX1 <ryopbun@gmail.com>
2025-05-31 00:42:15 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-echo "[+] Updating packages..."
-pkg update -y && pkg upgrade -y
-
-echo "[+] Installing Python and pip..."
+pkg update -y
 pkg install python -y
-
-echo "[+] Installing required Python modules..."
 pip install -r requirements.txt
-
-echo "[✓] Instalasi selesai!"
-echo "[✓] Jalankan: python3 fuzzscrape.py --help"
+echo "[✔] Install selesai!"
